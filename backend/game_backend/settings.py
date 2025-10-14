3f8eda6f3c0a60cee191157e5105c32c40728b19
Shrikant Giri <shrikant.giri@coriolis.co.in>
2025-10-14 20:02:13 +0530

    updated requirements

--- backend/game_backend/settings.py
+++ backend/game_backend/settings.py
@@ -151,15 +151,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
 
 # Where collectstatic will put all files
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Optional: If you have custom static files
+# React build static files
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "frontend" / "build" / "static",
 ]
 
 # Default primary key field type
